35cf36baf48b9d9bc738412a9ddab28d3647df5c
Jeú <jeufrancosilva@gmail.com>
2022-09-21 13:14:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using (var ctx = new Context())
+{
+    var clientes = ctx.Clientes.ToList();
+
+    foreach (var item in clientes)
+    {
+        Console.WriteLine($"Código: {item.Id} - Nome: {item.Nome}");
+    }
+}
